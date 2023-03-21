bee78d5db9fc224d12b238e5fd55b14f5d8ecdc7
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-03-21 12:23:03 -0500

    projects updated

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,9 +1,5 @@
 import capture1 from "../img/projects/Captura1-min.PNG";
 import preview1 from "../img/projects/todolist-preview-min.gif";
-import capture2 from "../img/projects/Captura2-min.PNG";
-import preview2 from "../img/projects/valorantfa-preview-min.gif";
-import capture4 from "../img/projects/Captura4-min.PNG";
-import preview4 from "../img/projects/colombia-preview.gif";
 import capture7 from "../img/projects/Captura7.PNG";
 
 export const projects = [
@@ -30,46 +26,6 @@ export const projects = [
   },
   {
     id: 2,
-    en: {
-      name: "Valorant for all",
-      description: "Compilation of videos to improve in valorant.",
-      category: ["Web page", "Personal project"],
-      status: "Completed",
-    },
-    es: {
-      name: "Valorant para todos",
-      description: "Recopilación de videos para mejorar en valorant.",
-      category: ["Página web", "Proyecto personal"],
-      status: "Completado",
-    },
-    image: capture2,
-    preview: preview2,
-    demo: "https://valorantfa-client.vercel.app/",
-    github: "https://github.com/Orloxx23/valorantfa-client",
-    tags: ["React", "Youtube API"],
-  },
-  {
-    id: 3,
-    en: {
-      name: "Armed conflict in Colombia",
-      description: "History of the armed conflict in Colombia.",
-      category: ["Web page", "Personal project"],
-      status: "Completed",
-    },
-    es: {
-      name: "Conflicto armado en colombia",
-      description: "Historia del conflicto armado en colombia.",
-      category: ["Página web", "Proyecto personal"],
-      status: "Completado",
-    },
-    image: capture4,
-    preview: preview4,
-    demo: "https://orloxx23.github.io/Conflicto-Colombia/",
-    github: "https://github.com/Orloxx23/Conflicto-Colombia",
-    tags: ["React"],
-  },
-  {
-    id: 4,
     en: {
       name: "Valorant Stats",
       description: "It allows Valorant users to search for players and view their game history.",
