1c8eb7eb081fc9a6667c726b8675463a9acdb31d
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-04-05 17:38:03 -0500

    project updated

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,5 +1,4 @@
 import capture1 from "../img/projects/Captura1-min.PNG";
-import preview1 from "../img/projects/todolist-preview-min.gif";
 import capture7 from "../img/projects/Captura7.PNG";
 import capture2 from "../img/projects/Captura2.PNG";
 
@@ -8,34 +7,37 @@ export const projects = [
     id: 1,
     en: {
       name: "To do list",
-      description: "To-do list where you can create, search and delete tasks.",
+      description:
+        "Todo list where you can create, search and delete tasks. You also receive advice to improve your productivity with the help of artificial intelligence (GPT3).",
       category: ["Web application", "Course project"],
       status: "Completed",
     },
     es: {
       name: "Lista de tareas",
       description:
-        "Lista de tareas donde puedes crear, buscar y eliminar tareas.",
+        "Lista de tareas donde puedes crear, buscar y eliminar tareas. Tambien recibes concejos para mejorar tu productividad con ayuda de inteligencia artificial (GPT3).",
       category: ["Aplicación web", "Proyecto de curso"],
       status: "Completado",
     },
     image: capture1,
-    preview: preview1,
+    preview: capture1,
     demo: "https://orloxx23.github.io/todolist",
     github: "https://github.com/Orloxx23/todolist",
-    tags: ["React"],
+    tags: ["React", "OpenAI", "GPT3"],
   },
   {
     id: 2,
     en: {
       name: "Valorant Stats",
-      description: "It allows Valorant users to search for players and view their game history.",
+      description:
+        "It allows Valorant users to search for players and view their game history.",
       category: ["Web application", "Personal project"],
       status: "In progress",
     },
     es: {
       name: "Estadísticas de Valorant",
-      description: "Permite a los usuarios de Valorant buscar jugadores y ver su historial de juego.",
+      description:
+        "Permite a los usuarios de Valorant buscar jugadores y ver su historial de juego.",
       category: ["Aplicación web", "Proyecto personal"],
       status: "En desarrollo",
     },
@@ -49,13 +51,15 @@ export const projects = [
     id: 3,
     en: {
       name: "Taply",
-      description: "Is a remote control for Valorant. Select the game mode, modify the room and choose your favorite agent from your mobile device.",
+      description:
+        "Is a remote control for Valorant. Select the game mode, modify the room and choose your favorite agent from your mobile device.",
       category: ["Web page", "Personal project"],
       status: "In progress",
     },
     es: {
       name: "Taply",
-      description: "Es un control remoto para Valorant. Selecciona el modo de juego, modifica la sala y elige a tu agente favorito desde tu dispositivo móvil.",
+      description:
+        "Es un control remoto para Valorant. Selecciona el modo de juego, modifica la sala y elige a tu agente favorito desde tu dispositivo móvil.",
       category: ["Pagina web", "Proyecto personal"],
       status: "En desarrollo",
     },
